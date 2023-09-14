1f4aa75e9074546de24d92ac3c838cd1abd0bb14
Chibuike Mba <chibuike.code@gmail.com>
2023-09-14 01:46:54 +0100

    Apply Special Effects Design to the Float Layout

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let loginButton = document.getElementById("login");
 let spanElement = document.getElementById("user_id");
 let hamburgerUl = document.getElementById("hamburger_ul");
 let hamburgerButton = document.getElementById("hamburger_button");
+let toggleSwitchLarge = document.getElementById("checkme_large");
+let toggleSwitchMobile = document.getElementById("checkme_mobile");
+let htmlElement = document.getElementsByTagName("html");
+let userSvg = document.getElementById("user_svg");
 let webPage = window.location.href;
 
 if (submitButton !== null)
@@ -22,7 +26,9 @@ if (submitButton !== null)
 		let formInputs = document.querySelectorAll("input");
 		let feedback = document.getElementById("feedback");
 
-		for (let i = 0; i < queryArray.length; i++) 
+		console.log(formInputs);
+
+		for (let i = 2; i < queryArray.length; i++) 
 		{	
 			if (formInputs[i].validity.valueMissing === false)
 			{
@@ -30,7 +36,7 @@ if (submitButton !== null)
 			}
 		}
 
-		if (formInputs[2].value !== formInputs[3].value) 
+		if (formInputs[4].value !== formInputs[5].value) 
 		{
 			feedback.textContent = "Passwords don't match!";
 			feedback.removeAttribute("class");
@@ -50,9 +56,9 @@ if (loginButton !== null)
         let storedPassword = localStorage.getItem("pword");
         let storedEmail = localStorage.getItem("email_address");
 
-		if (formInputs[0].validity.valueMissing === false && formInputs[1].validity.valueMissing === false)
+		if (formInputs[2].validity.valueMissing === false && formInputs[3].validity.valueMissing === false)
 		{
-			if (storedEmail === formInputs[0].value && storedPassword === formInputs[1].value)
+			if (storedEmail === formInputs[2].value && storedPassword === formInputs[3].value)
 			{
 				location.href = "./dashboard.html";
 			}
@@ -93,7 +99,7 @@ let dashboardData =
 	chngeYourUnameInput: document.getElementById("username_id"),
 	chngeYourUnameSubmitButton: document.getElementById("submit_id"),
 	returnSubmitMsg: document.getElementById("return_info"),
-	timeInsert: document.getElementById("time_insert")
+	timeInsert: document.getElementById("time_insert"),
 }
 
 if (dashboardData.storeJSONButton !== null)
@@ -180,4 +186,180 @@ if (hamburgerButton !== null)
 	hamburgerButton.addEventListener("click", function() {
 		hamburgerUl.classList.toggle("toggle");
 	});
+}
+
+
+if (toggleSwitchMobile !== null) 
+{
+	toggleSwitchMobile.addEventListener("click", function() {
+
+		if (localStorage.getItem("displayModeMobile") === "dark_mode") {
+			localStorage.setItem("displayModeMobile", "change_display");
+		}
+		else 
+		{
+			localStorage.setItem("displayModeMobile", "dark_mode");
+		}
+
+		if (localStorage.getItem("displayModeMobile") === "change_display")
+		{
+			htmlElement[0].setAttribute("class", localStorage.getItem("displayModeMobile"));
+		
+			let hamburgerImgSrc = hamburgerButton.getAttribute("src");
+		
+				if (hamburgerImgSrc === "./svg/hamburger.svg") 
+				{
+					hamburgerButton.setAttribute("src", "./svg/hamburger_dark.svg");
+					
+					const spanBackground = document.getElementById("span_id_mobile");
+					spanBackground.setAttribute("class", "span_dark_bkd");
+				}
+				else
+				{
+					hamburgerButton.setAttribute("src", "./svg/hamburger.svg");
+
+					const spanBackground = document.getElementById("span_id_mobile");
+					spanBackground.setAttribute("class", "span_light_bkd");
+				}
+		}
+		else 
+		{
+			htmlElement[0].removeAttribute("class");
+			let hamburgerImgSrc = hamburgerButton.getAttribute("src");
+		
+				if (hamburgerImgSrc === "./svg/hamburger.svg") 
+				{
+					hamburgerButton.setAttribute("src", "./svg/hamburger_dark.svg");
+
+					const spanBackground = document.getElementById("span_id_mobile");
+					spanBackground.setAttribute("class",  "span_dark_bkd");
+				}
+				else
+				{
+					hamburgerButton.setAttribute("src", "./svg/hamburger.svg");
+
+					const spanBackground = document.getElementById("span_id_mobile");
+					spanBackground.setAttribute("class", "span_light_bkd");
+				}
+		}
+
+	});
+}
+
+if (window.innerWidth <= 850)
+{
+	if (localStorage.getItem("displayModeMobile") === "change_display")
+	{
+		htmlElement[0].setAttribute("class", localStorage.getItem("displayModeMobile"));
+
+			let hamburgerImgSrc = hamburgerButton.getAttribute("src");
+
+			if (hamburgerImgSrc === "./svg/hamburger.svg") 
+			{
+				hamburgerButton.setAttribute("src", "./svg/hamburger_dark.svg");
+
+				const spanBackground = document.getElementById("span_id_mobile");
+				spanBackground.setAttribute("class",  "span_dark_bkd");
+			}
+			else
+			{
+				hamburgerButton.setAttribute("src", "./svg/hamburger.svg");
+
+				const spanBackground = document.getElementById("span_id_mobile");
+				spanBackground.setAttribute("class", "span_light_bkd");
+			}
+	}
+	else 
+	{
+		const spanBackground = document.getElementById("span_id_mobile");
+		spanBackground.setAttribute("class", "span_light_bkd");
+	}
+}
+
+if (toggleSwitchLarge !== null) 
+{
+	toggleSwitchLarge.addEventListener("click", function() {
+
+		if (localStorage.getItem("displayModeLarge") === "dark_mode") {
+			localStorage.setItem("displayModeLarge", "change_display");
+		}
+		else 
+		{
+			localStorage.setItem("displayModeLarge", "dark_mode");
+		}
+
+		if (localStorage.getItem("displayModeLarge") === "change_display")
+		{
+			htmlElement[0].setAttribute("class", localStorage.getItem("displayModeLarge"));
+		
+			let userImgSrc = userSvg.getAttribute("src");
+		
+				if (userImgSrc === "./svg/user.svg") 
+				{
+					userSvg.setAttribute("src", "./svg/user_dark.svg");
+					
+					const spanBackground = document.getElementById("span_id_large");
+					spanBackground.setAttribute("class",  "span_dark_bkd");
+				}
+				else
+				{
+					userSvg.setAttribute("src", "./svg/user.svg");
+
+					const spanBackground = document.getElementById("span_id_large");
+					spanBackground.setAttribute("class",  "span_light_bkd");
+				}
+		}
+		else 
+		{
+			htmlElement[0].removeAttribute("class");
+			let userImgSrc = userSvg.getAttribute("src");
+		
+				if (userImgSrc === "./svg/user.svg") 
+				{
+					userSvg.setAttribute("src", "./svg/user_dark.svg");
+
+					const spanBackground = document.getElementById("span_id_large");
+					spanBackground.setAttribute("class",  "span_dark_bkd");
+				}
+				else
+				{
+					userSvg.setAttribute("src", "./svg/user.svg");
+
+					const spanBackground = document.getElementById("span_id_large");
+					spanBackground.setAttribute("class",  "span_light_bkd");
+				}
+		}
+
+	});
+}
+
+
+if (window.innerWidth > 850)
+{
+	if (localStorage.getItem("displayModeLarge") === "change_display")
+	{
+		htmlElement[0].setAttribute("class", localStorage.getItem("displayModeLarge"));
+
+			let userImgSrc = userSvg.getAttribute("src");
+
+			if (userImgSrc === "./svg/user.svg") 
+			{
+				userSvg.setAttribute("src", "./svg/user_dark.svg");
+
+				const spanBackground = document.getElementById("span_id_large");
+				spanBackground.setAttribute("class",  "span_dark_bkd");
+			}
+			else
+			{
+				userSvg.setAttribute("src", "./svg/user.svg");
+
+				const spanBackground = document.getElementById("span_id_large");
+				spanBackground.setAttribute("class",  "span_light_bkd");
+			}
+	}
+	else
+	{
+		const spanBackground = document.getElementById("span_id_large");
+		spanBackground.setAttribute("class",  "span_light_bkd");
+	}
 }
